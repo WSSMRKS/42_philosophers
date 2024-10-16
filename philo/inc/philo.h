8b5aad25c40c3ae751609e305edbc86a6d82fc44
Markus Weiss <markusweiss.mw@gmail.com>
2024-10-16 17:58:47 +0200

    removed comments

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: maweiss <maweiss@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 14:36:24 by maweiss           #+#    #+#             */
-/*   Updated: 2024/10/09 16:14:31 by maweiss          ###   ########.fr       */
+/*   Updated: 2024/10/16 17:41:32 by maweiss          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,35 +33,35 @@ typedef struct s_general	t_general;
 typedef struct s_philosopher
 {
 	int						philo_nb;
-	long long				startup_time;		//local startup time;
-	long					last_meal_time;		//MUTEX
-	long					nbothe;				//Number of times has eaten
+	long long				startup_time;
+	long					last_meal_time;
+	long					nbothe;
 	t_general				*main;
-	pthread_mutex_t			right_fork;			//MUTEX
-	pthread_mutex_t			*left_fork;			//MUTEX
-	pthread_mutex_t			meal_count;			//MUTEX
-	pthread_mutex_t			time;				//MUTEX
+	pthread_mutex_t			right_fork;
+	pthread_mutex_t			*left_fork;
+	pthread_mutex_t			meal_count;
+	pthread_mutex_t			time;
 }				t_philosopher;
 
 typedef struct s_general
 {
 	int				nb_philo;
-	int				ttd;				//Time to die;
+	int				ttd;
 	int				ttd2;
-	int				tte;				//Time to eat;
+	int				tte;
 	int				tte2;
-	int				tts;				//Time to sleep;
+	int				tts;
 	int				tts2;
-	int				nbotte;				//Number of times to eat;
-	bool			nbotte_present;		//Number of times to eat Present?;
+	int				nbotte;
+	bool			nbotte_present;
 	pthread_t		**threads;
 	void			**thread_return;
 	int				philos_spawned;
 	long long		startup_time;
-	bool			death_occured;		//MUTEXed
+	bool			death_occured;
 	t_philosopher	**philos;
-	pthread_mutex_t	death;				//MUTEX
-	pthread_mutex_t	print;				//MUTEX
+	pthread_mutex_t	death;
+	pthread_mutex_t	print;
 }				t_general;
 
 /* function prototypes*/
